03d320ee741e375c1700b13dbe11066fc1d3dee9
ProgDaniel <43956461+ProgDaniel@users.noreply.github.com>
2019-10-04 15:03:36 +0200

    Address klasse + constructors

--- HasserisWeb/Model/Customer.cs
+++ HasserisWeb/Model/Customer.cs
@@ -9,20 +9,20 @@ namespace HasserisWeb
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public int ID { get; private set; }
-        private Address Address { get; set; }
+        public int ID { get; set; }
+        public Address Address { get; set; }
+        public Customer(string fname, string lname, int id, Address address)
+        {
+            this.FirstName = fname;
+            this.LastName = lname;
+            this.ID = id;
+            this.Address = address;
+        }
     }
 
     public class Business : Customer
     {
         public int EAN { get; set; }
         public int CVR { get; set; }
-    }
-
-    internal class Address
-    {
-        public int ZIP { get; set; }
-        public string City { get; set; }
-        public string Note { get; set; }
     }
 }
