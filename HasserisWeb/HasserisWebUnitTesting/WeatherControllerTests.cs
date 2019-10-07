a084a537e18eddae8d10ddd3c8a1244a9a14322c
Christoffer <chollensen1999@gmail.com>
2019-10-07 12:14:04 +0200

    First commit

---
+++ HasserisWeb/HasserisWebUnitTesting/WeatherControllerTests.cs
@@ -0,0 +1,15 @@
+using NUnit.Framework;
+
+namespace HasserisWebUnitTesting
+{
+    [TestFixture]
+    public class WeatherControllerTests
+    {
+        [Test]
+        public void Test_Blah_ReturnsYes()
+        {
+            
+        }
+        
+    }
+}
