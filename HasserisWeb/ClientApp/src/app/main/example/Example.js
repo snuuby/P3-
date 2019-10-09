ef4ea6ee1dc1a56e8670269255d7a1592630c96a
Christoffer <chollensen1999@gmail.com>
2019-10-09 18:51:08 +0200

    2) Started on controller - stable

--- HasserisWeb/ClientApp/src/app/main/example/Example.js
+++ HasserisWeb/ClientApp/src/app/main/example/Example.js
@@ -6,10 +6,54 @@ const styles = theme => ({
     layoutRoot: {}
 });
 
+
+
 class Example extends Component {
 
+    // Test af controller, Constructor er lavet af Cholle
+    constructor(props) {
+        super(props);
+        this.state = { forecasts: [], loading: true };
+    }
+    
+    // Cholle
+    componentDidMount() {
+        this.populateWeatherData();
+    }
+    
+    // Cholle
+    static renderForecastsTable(forecasts) {
+        return (
+            <table className='table table-striped' aria-labelledby="tabelLabel">
+                <thead>
+                <tr>
+                    <th>Date</th>
+                    <th>Temp. (C)</th>
+                    <th>Temp. (F)</th>
+                    <th>Summary</th>
+                </tr>
+                </thead>
+                <tbody>
+                {forecasts.map(forecast =>
+                    <tr key={forecast.date}>
+                        <td>{forecast.date}</td>
+                        <td>{forecast.temperatureC}</td>
+                        <td>{forecast.temperatureF}</td>
+                        <td>{forecast.summary}</td>
+                    </tr>
+                )}
+                </tbody>
+            </table>
+        );
+    }
+    
     render()
     {
+        // Cholle
+        let contents = this.state.loading
+            ? <p><em>Loading...</em></p>
+            : Example.renderForecastsTable(this.state.forecasts);
+        
         const {classes} = this.props;
         return (
             <FusePageSimple
@@ -24,6 +68,13 @@ class Example extends Component {
                 }
                 content={
                     <div className="p-24">
+                        <h4>Controller data - Cholle</h4>
+                        <div>
+                            <h1 id="tabelLabel" >Weather forecast</h1>
+                            <p>This component demonstrates fetching data from the server.</p>
+                            {contents}
+                        </div>
+                        
                         <h4>Content</h4>
                         <br/>
                         <DemoContent/>
@@ -32,6 +83,14 @@ class Example extends Component {
             />
         )
     }
+    
+    // Cholle
+    async populateWeatherData() {
+        const response = await fetch('weatherforecast');
+        const data = await response.json();
+        this.setState({ forecasts: data, loading: false });
+    }
+    
 }
 
 export default withStyles(styles, {withTheme: true})(Example);
