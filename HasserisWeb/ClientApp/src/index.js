6a6ce8631246bf36032564d9117056b832c595e3
snuuby <jallehansen17@gmaill.com>
2019-10-14 15:43:11 +0200

    removed shit bootstrap

--- HasserisWeb/ClientApp/src/index.js
+++ HasserisWeb/ClientApp/src/index.js
@@ -12,7 +12,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import App from 'app/App';
-import 'bootstrap/dist/css/bootstrap.css';
 
 
 // Her tilføjes App som helhed som skal renders
