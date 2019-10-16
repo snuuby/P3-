9377708e3d250c1ccd19892428e1a13f19e3441d
Jakob Østenkjær Hansen <jallehansen17@gmail.com>
2019-10-16 02:15:49 +0200

    Made some more changes to the login system
    Need to send info back to backend to verify. Also working on a solution to remember if a user is logged in, and also re-route once he is logged in.

--- HasserisWeb/ClientApp/src/app/main/login/Login.js
+++ HasserisWeb/ClientApp/src/app/main/login/Login.js
@@ -1,4 +1,4 @@
-﻿import React, { Component, useState } from 'react';
+import React, { Component, useState } from 'react';
 import {withStyles} from '@material-ui/core/styles';
 import {FusePageSimple, DemoContent} from '@fuse';
 import { makeStyles, ThemeProvider } from '@material-ui/styles';
@@ -9,121 +9,25 @@ import { Link } from 'react-router-dom';
 import clsx from 'clsx';
 import { TextField, Button, Dialog, DialogActions, DialogContent, Icon, IconButton, Toolbar, AppBar, FormControlLabel, Switch } from '@material-ui/core';
 import 'styles/index.css';
+import LoginDialog from './LoginDialog';
 const styles = theme => ({
     layoutRoot: {}
 });
-
-
 
 class Login extends Component {
 
     constructor(props) {
         super(props);
-        this.state = { username: '', password: '', error: '', open: true };
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.dismissError = this.dismissError.bind(this);
-        this.handleUserChange = this.handleUserChange.bind(this);
-        this.handlePassChange = this.handlePassChange.bind(this);
-            }
-
-    dismissError() {
-        this.setState({ error: '' });
+        this.state = {loggedIn: false}
     }
 
-    handleSubmit(evt) {
-
-        if (!this.state.username) {
-            this.setState({ error: 'Du skal have et brugernavn. Kontakt din adminstrator hvis du ikke kan huske dit brugernavn' });
-        }
-
-        if (!this.state.password) {
-            this.setState({ error: 'Du skal have et password. Kontakt din adminstrator hvis du ikke kan huske dit password' });
-        }
-    return this.setState({ error: 'Ukendt fejl' });
-
-}
-
-
-    handleUserChange(evt) {
-        this.setState({
-            username: evt.target.value,
-        });
-    };
-
-    handlePassChange(evt) {
-        this.setState({
-            password: evt.target.value,
-        });
+    toggleLogin = () => {
+        this.setState(state => ({loggedIn: !state.loggedIn}))
     }
-
-
-    render()
-    {
-        const {classes} = this.props;
-        
-        return (
-            <div>
-                <Dialog open={this.state.open} fullWidth maxWidth='xs' component="form">
-
-                <AppBar position="static">
-                    <Toolbar className="flex w-full">
-                        <Typography variant="subtitle1" color="inherit">
-                            Login
-                        </Typography>
-                    </Toolbar>
-                </AppBar>
-
-                <form>
-                    <DialogContent classes={{ root: "p-16 pb-0 sm:p-24 sm:pb-0" }}>
-                    
-                    <TextField
-                        label='Brugernavn'
-                        id='brugernavn'
-                        onChange={this.handleUserChange}
-                        value={this.state.username}
-                            error={this.state.error}
-                            margin='normal'
-                            variant='outlined'
-                        />
-
-                    <TextField
-
-                        label='Password'
-                        id= 'password'
-                        onChange={this.handleUserChange}
-                        value={this.state.password}
-                            error={this.state.error}
-                            margin='normal'
-                            variant='outlined'
-                        />
-
-                    </DialogContent>
-                        <DialogActions className="justify-between pl-8 sm:pl-16">
-                            <Button
-                                variant="contained"
-                                color="primary"
-                                type="submit"
-                                onClick={() => this.handleSubmit}
-                                >Login
-                           </Button>
-                        </DialogActions>
-
-                </form>
-                </Dialog>
-                </div>
-            )
-        }
-      handleUserChange(evt) {
-    this.setState({
-      username: evt.target.value,
-    });
-  };
-
-  handlePassChange(evt) {
-    this.setState({
-      password: evt.target.value,
-    });
-  }
+    render() {
+        return(
+            this.state.loggedIn ? null : <LoginDialog toggleLogin={this.toggleLogin}/>
+        )
+    }
 }
-
 export default withStyles(styles, {withTheme: true})(Login);
