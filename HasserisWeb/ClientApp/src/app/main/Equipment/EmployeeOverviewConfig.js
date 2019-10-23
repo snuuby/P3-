a548bff6bf4e2bec90229b8192d8b0e13a591f7f
Helledie <44023179+Helledie@users.noreply.github.com>
2019-10-23 15:26:28 +0200

    kopi

---
+++ HasserisWeb/ClientApp/src/app/main/Equipment/EmployeeOverviewConfig.js
@@ -0,0 +1,39 @@
+import EmployeeOverview from './EmployeeOverview';
+import React from "react";
+import authRoles from "../../auth/authRoles";
+
+export const EmployeeOverviewConfig = {
+    settings: {
+        layout: {
+            config: {}
+        }
+    },
+    auth    : authRoles.adminPlus,
+    routes  : [
+        {
+            path     : '/employee/overview',
+            component: React.lazy(() => import('./EmployeeOverview'))
+        }
+    ]
+};
+
+/**
+ * Lazy load Example
+ */
+/*
+import React from 'react';
+
+export const ExampleConfig = {
+    settings: {
+        layout: {
+            config: {}
+        }
+    },
+    routes  : [
+        {
+            path     : '/example',
+            component: React.lazy(() => import('./Example'))
+        }
+    ]
+};
+*/
